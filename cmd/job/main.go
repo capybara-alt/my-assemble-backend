3c9dc83ff25e4768607998f288453c023636e40e
Reinker <rencker3@gmail.com>
2023-10-18 00:49:58 +0900

    crawlersの変更に対応し、mainを変更

--- cmd/job/main.go
+++ cmd/job/main.go
@@ -7,8 +7,9 @@ import (
 
 	convert "github.com/capybara-alt/my-assemble/convert/job"
 	"github.com/capybara-alt/my-assemble/core"
-	"github.com/capybara-alt/my-assemble/crawler"
+	"github.com/capybara-alt/my-assemble/infrastructure/crawler"
 	repo "github.com/capybara-alt/my-assemble/infrastructure/db"
+	"github.com/capybara-alt/my-assemble/repository"
 	"github.com/capybara-alt/my-assemble/usecase/common"
 	"github.com/capybara-alt/my-assemble/usecase/job"
 	"gorm.io/driver/postgres"
@@ -39,44 +40,44 @@ func main() {
 	validation_schema.Execute(ctx)
 
 	weapon_convert := convert.NewWeaponList(validation_schema.GetWeaponSchema())
-	var weapon_crawlers = []crawler.ICrawler{
-		crawler.NewCrossWeaponCrawler(),
-		crawler.NewAmmoWeaponCrawler(),
-		crawler.NewEnCoralWeaponCrawler(),
-		crawler.NewExplodeWeaponCrawler(),
-		crawler.NewExtraWeaponCrawler(),
-		crawler.NewLauncherWeaponCrawler(),
-		crawler.NewMissileWeaponCrawler(),
-		crawler.NewOrbitTaletDroneWeaponCrawler(),
-		crawler.NewShieldWeaponCrawler(),
+	var external_weapon_repo = []repository.ExternalWeapon{
+		crawler.NewCrossWeapon(),
+		crawler.NewAmmoWeapon(),
+		crawler.NewEnCoralWeapon(),
+		crawler.NewExplodeWeapon(),
+		crawler.NewExtraWeapon(),
+		crawler.NewLauncherWeapon(),
+		crawler.NewMissileWeapon(),
+		crawler.NewOrbitTaletDroneWeapon(),
+		crawler.NewShieldWeapon(),
 	}
 	weapon_repo := repo.NewWeapon()
-	job.NewWeaponJob(weapon_crawlers, weapon_repo, weapon_convert, logger).Execute(ctx)
+	job.NewWeaponJob(weapon_repo, external_weapon_repo, weapon_convert, logger).Execute(ctx)
 
 	frame_convert := convert.NewFrameList(validation_schema.GetFrameSchema())
-	var frame_crawlers = []crawler.ICrawler{
-		crawler.NewArmsFrameCrawler(),
-		crawler.NewCoreFrameCrawler(),
-		crawler.NewHeadFrameCrawler(),
-		crawler.NewLegsFrameCrawler(),
-		crawler.NewOtherLegsFrameCrawler(),
+	var external_frame_repo = []repository.ExternalFrame{
+		crawler.NewArmsFrame(),
+		crawler.NewCoreFrame(),
+		crawler.NewHeadFrame(),
+		crawler.NewLegsFrame(),
+		crawler.NewOtherLegsFrame(),
 	}
 	frame_repo := repo.NewFrame()
-	job.NewFrameJob(frame_crawlers, frame_repo, frame_convert, logger).Execute(ctx)
+	job.NewFrameJob(frame_repo, external_frame_repo, frame_convert, logger).Execute(ctx)
 
 	inner_unit_convert := convert.NewInnerUnitsList(validation_schema.GetInnerUnitsSchema())
-	var inner_unit_crawlers = []crawler.ICrawler{
-		crawler.NewBoosterInnerUnitCrawler(),
-		crawler.NewFcsInnerUnitCrawler(),
-		crawler.NewGeneratorInnerUnitCrawler(),
+	var external_inner_units_repo = []repository.ExternalInnerUnit{
+		crawler.NewBoosterInnerUnit(),
+		crawler.NewFcsInnerUnit(),
+		crawler.NewGeneratorInnerUnit(),
 	}
 	inner_unit_repo := repo.NewInnerUnit()
-	job.NewInnerUnitJob(inner_unit_crawlers, inner_unit_repo, inner_unit_convert, logger).Execute(ctx)
+	job.NewInnerUnitJob(inner_unit_repo, external_inner_units_repo, inner_unit_convert, logger).Execute(ctx)
 
 	expansion_convert := convert.NewExpansionList(validation_schema.GetExpansionSchema())
-	var expansion_crawlers = []crawler.ICrawler{
-		crawler.NewExpansionCrawler(),
+	var external_expansion_repo = []repository.ExternalExpansion{
+		crawler.NewExpansion(),
 	}
 	expansion_repo := repo.NewExpansion()
-	job.NewExpansionJob(expansion_crawlers, expansion_repo, expansion_convert, logger).Execute(ctx)
+	job.NewExpansionJob(expansion_repo, external_expansion_repo, expansion_convert, logger).Execute(ctx)
 }
