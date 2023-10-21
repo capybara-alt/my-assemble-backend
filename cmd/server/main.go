8e0149bb2bb4796ced4fc6bf2c93d2429fc29abd
Reinker <rencker3@gmail.com>
2023-10-21 22:09:58 +0900

    call handlers in entry point

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,47 @@
 package main
 
-import "log"
+import (
+	"log/slog"
+	"net/http"
+	"os"
+
+	"buf.build/gen/go/capybara/my-assemble/connectrpc/go/myassembly/v1/myassemblyv1connect"
+	"connectrpc.com/connect"
+	"github.com/capybara-alt/my-assemble/handler"
+	repo "github.com/capybara-alt/my-assemble/infrastructure/db"
+	"github.com/capybara-alt/my-assemble/interceptor"
+	"golang.org/x/net/http2"
+	"golang.org/x/net/http2/h2c"
+)
 
 func main() {
-	log.Println("Hello world!")
+	logger := slog.New(slog.NewJSONHandler(os.Stdout, nil))
+
+	mux := http.NewServeMux()
+
+	interceptors := connect.WithInterceptors(interceptor.NewInitConnectionInterceptor())
+	expansionRepo := repo.NewExpansion()
+	frameRepo := repo.NewFrame()
+	innerUnitRepo := repo.NewInnerUnit()
+	weaponRepo := repo.NewWeapon()
+	mux.Handle(myassemblyv1connect.NewExpansionServiceHandler(
+		handler.NewExpansionHandler(expansionRepo, logger),
+		interceptors,
+	))
+	mux.Handle(myassemblyv1connect.NewFrameServiceHandler(
+		handler.NewFrameHandler(frameRepo),
+		interceptors,
+	))
+	mux.Handle(myassemblyv1connect.NewInnerUnitServiceHandler(
+		handler.NewInnerUnitHandler(innerUnitRepo),
+		interceptors,
+	))
+	mux.Handle(myassemblyv1connect.NewWeaponServiceHandler(
+		handler.NewWeaponHandler(weaponRepo, logger),
+		interceptors,
+	))
+
+	logger.Info("Start server")
+	err := http.ListenAndServe("0.0.0.0:8080", h2c.NewHandler(mux, &http2.Server{}))
+	logger.Error("Failed to start server", "detail", err)
 }
