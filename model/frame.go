8750e3d3804682559ec935dc57e073ef39c36840
Reinker <rencker3@gmail.com>
2023-10-21 22:08:26 +0900

    add convert function in model

--- model/frame.go
+++ model/frame.go
@@ -1,6 +1,10 @@
 package model
 
-import "time"
+import (
+	"time"
+
+	myassemblyv1 "buf.build/gen/go/capybara/my-assemble/protocolbuffers/go/myassembly/v1"
+)
 
 type Frame struct {
 	Name                  string `gorm:"primaryKey"`
@@ -42,4 +46,92 @@ type Frame struct {
 	Weight                int64
 	HighSpeedPref         int64
 	CreatedAt             time.Time
+}
+
+func (f *Frame) ToPB() *myassemblyv1.Frame {
+	return &myassemblyv1.Frame{
+		Name:                  f.Name,
+		Category:              f.Category,
+		Unit:                  f.Unit,
+		AbThrust:              int32(f.ABThrust),
+		AbEnConsumption:       int32(f.AbEnConsumption),
+		Ap:                    int32(f.AP),
+		EnLoad:                int32(f.ENLoad),
+		QbReloadIdealWeight:   int32(f.QBReloadIdealWeight),
+		QbReloadTime:          float32(f.QBReloadTime),
+		QbJetDuration:         float32(f.QBJetDuration),
+		QbThrust:              int32(f.QBThrust),
+		QbEnConsumption:       int32(f.QbEnConsumption),
+		SystemRecovery:        int32(f.SystemRecovery),
+		GeneratorSupplyAdj:    int32(f.GeneratorSupplyAdj),
+		GeneratorOutputAdj:    int32(f.GeneratorOutputAdj),
+		ScanStandbyTime:       float32(f.ScanStandbyTime),
+		ScanEffectDuration:    float32(f.ScanEffectDuration),
+		ScanDistance:          int32(f.ScanDistance),
+		BoosterEfficiencyAdj:  int32(f.BoosterEfficiencyAdj),
+		Maker:                 f.Maker,
+		UpwardThrust:          int32(f.UpwardThrust),
+		UpwardEnConsumption:   int32(f.UpwardENConsumption),
+		Price:                 int32(f.Price),
+		RecoilControl:         int32(f.RecoilControl),
+		JumpHeight:            int32(f.JumpHeight),
+		AttitudeStability:     int32(f.AttitudeStability),
+		FirearmSpecialization: int32(f.FirearmSpecialization),
+		Thrust:                int32(f.Thrust),
+		JumpDistance:          int32(f.JumpDistance),
+		LoadLimit:             int32(f.LoadLimit),
+		AntiEnergyDefense:     int32(f.AntiEnergyDefense),
+		AntiKineticDefense:    int32(f.AntiKineticDefense),
+		AntiExplosiveDefense:  int32(f.AntiExplosiveDefense),
+		ArmsLoadLimit:         int32(f.ArmsLoadLimit),
+		TravelSpeed:           int32(f.TravelSpeed),
+		MeleeSpecialization:   int32(f.MeleeSpecialization),
+		Weight:                int32(f.Weight),
+		HighSpeedPref:         int32(f.HighSpeedPref),
+		CreatedAt:             f.CreatedAt.UnixMilli(),
+	}
+}
+
+func (f *Frame) FromPB(frame *myassemblyv1.Frame) *Frame {
+	return &Frame{
+		Name:                  frame.Name,
+		Category:              frame.Category,
+		Unit:                  frame.Unit,
+		ABThrust:              int64(frame.AbThrust),
+		AbEnConsumption:       int64(frame.AbEnConsumption),
+		AP:                    int64(frame.Ap),
+		ENLoad:                int64(frame.EnLoad),
+		QBReloadIdealWeight:   int64(frame.QbReloadIdealWeight),
+		QBReloadTime:          float64(frame.QbReloadTime),
+		QBJetDuration:         float64(frame.QbJetDuration),
+		QBThrust:              int64(frame.QbThrust),
+		QbEnConsumption:       int64(frame.QbEnConsumption),
+		SystemRecovery:        int64(frame.SystemRecovery),
+		GeneratorSupplyAdj:    int64(frame.GeneratorSupplyAdj),
+		GeneratorOutputAdj:    int64(frame.GeneratorOutputAdj),
+		ScanStandbyTime:       float64(frame.ScanStandbyTime),
+		ScanEffectDuration:    float64(frame.ScanEffectDuration),
+		ScanDistance:          int64(frame.ScanDistance),
+		BoosterEfficiencyAdj:  int64(frame.BoosterEfficiencyAdj),
+		Maker:                 frame.Maker,
+		UpwardThrust:          int64(frame.UpwardThrust),
+		UpwardENConsumption:   int64(frame.UpwardEnConsumption),
+		Price:                 int64(frame.Price),
+		RecoilControl:         int64(frame.RecoilControl),
+		JumpHeight:            int64(frame.JumpHeight),
+		AttitudeStability:     int64(frame.AttitudeStability),
+		FirearmSpecialization: int64(frame.FirearmSpecialization),
+		Thrust:                int64(frame.Thrust),
+		JumpDistance:          int64(frame.JumpDistance),
+		LoadLimit:             int64(frame.LoadLimit),
+		AntiEnergyDefense:     int64(frame.AntiEnergyDefense),
+		AntiKineticDefense:    int64(frame.AntiKineticDefense),
+		AntiExplosiveDefense:  int64(frame.AntiExplosiveDefense),
+		ArmsLoadLimit:         int64(frame.ArmsLoadLimit),
+		TravelSpeed:           int64(frame.TravelSpeed),
+		MeleeSpecialization:   int64(frame.MeleeSpecialization),
+		Weight:                int64(frame.Weight),
+		HighSpeedPref:         int64(frame.HighSpeedPref),
+		CreatedAt:             time.UnixMilli(frame.CreatedAt),
+	}
 }
--- model/inner_unit.go
+++ model/inner_unit.go
@@ -1,6 +1,10 @@
 package model
 
-import "time"
+import (
+	"time"
+
+	myassemblyv1 "buf.build/gen/go/capybara/my-assemble/protocolbuffers/go/myassembly/v1"
+)
 
 type InnerUnit struct {
 	Name                  string `gorm:"primaryKey"`
@@ -34,4 +38,76 @@ type InnerUnit struct {
 	LogRangeAssist        int64
 	Weight                int64
 	CreatedAt             time.Time
+}
+
+func (i *InnerUnit) ToPB() *myassemblyv1.InnerUnit {
+	return &myassemblyv1.InnerUnit{
+		Name:                  i.Name,
+		Category:              i.Category,
+		Unit:                  i.Unit,
+		Maker:                 i.Maker,
+		AbThrust:              int32(i.ABThrust),
+		AbEnConsumption:       int32(i.AbEnConsumption),
+		EnOutput:              int32(i.ENOutput),
+		EnCapacity:            int32(i.ENCapacity),
+		EnergyFirearmSpec:     int32(i.EnergyFirearmSpec),
+		EnRecharge:            int32(i.ENRecharge),
+		EnLoad:                int32(i.ENLoad),
+		QbReloadIdealWeight:   int32(i.QBReloadIdealWeight),
+		QbJetDuration:         float32(i.QBJetDuration),
+		QbThrust:              int32(i.QBThrust),
+		QbReloadTime:          float32(i.QBReloadTime),
+		QbEnConsumption:       int32(i.QbEnConsumption),
+		UpwardThrust:          int32(i.UpwardThrust),
+		UpwardEnConsumption:   int32(i.UpwardENConsumption),
+		MediumRangeAssist:     int32(i.MediumRangeAssist),
+		SupplyRecovery:        int32(i.SupplyRecovery),
+		Price:                 int32(i.Price),
+		PostRecoveryEnSupply:  int32(i.PostRecoveryENSupply),
+		Thrust:                int32(i.Thrust),
+		MissileLockCorrection: int32(i.MissileLockCorrection),
+		MultiLockCorrection:   int32(i.MultiLockCorrection),
+		MeleeAttackThrust:     int32(i.MeleeAttackThrust),
+		MeleeAtkEnConsump:     int32(i.MeleeAtkENConsump),
+		CloseRangeAssist:      int32(i.CloseRangeAssist),
+		LogRangeAssist:        int32(i.LogRangeAssist),
+		Weight:                int32(i.Weight),
+		CreatedAt:             i.CreatedAt.UnixMilli(),
+	}
+}
+
+func (i *InnerUnit) FromPB(innerUnit *myassemblyv1.InnerUnit) *InnerUnit {
+	return &InnerUnit{
+		Name:                  innerUnit.Name,
+		Category:              innerUnit.Category,
+		Unit:                  innerUnit.Unit,
+		Maker:                 innerUnit.Maker,
+		ABThrust:              int64(innerUnit.AbThrust),
+		AbEnConsumption:       int64(innerUnit.AbEnConsumption),
+		ENOutput:              int64(innerUnit.EnOutput),
+		ENCapacity:            int64(innerUnit.EnCapacity),
+		EnergyFirearmSpec:     int64(innerUnit.EnergyFirearmSpec),
+		ENRecharge:            int64(innerUnit.EnRecharge),
+		ENLoad:                int64(innerUnit.EnLoad),
+		QBReloadIdealWeight:   int64(innerUnit.QbReloadIdealWeight),
+		QBJetDuration:         float64(innerUnit.QbJetDuration),
+		QBThrust:              int64(innerUnit.QbThrust),
+		QBReloadTime:          float64(innerUnit.QbReloadTime),
+		QbEnConsumption:       int64(innerUnit.QbEnConsumption),
+		UpwardThrust:          int64(innerUnit.UpwardThrust),
+		UpwardENConsumption:   int64(innerUnit.UpwardEnConsumption),
+		MediumRangeAssist:     int64(innerUnit.MediumRangeAssist),
+		SupplyRecovery:        int64(innerUnit.SupplyRecovery),
+		Price:                 int64(innerUnit.Price),
+		PostRecoveryENSupply:  int64(innerUnit.PostRecoveryEnSupply),
+		Thrust:                int64(innerUnit.Thrust),
+		MissileLockCorrection: int64(innerUnit.MissileLockCorrection),
+		MultiLockCorrection:   int64(innerUnit.MultiLockCorrection),
+		MeleeAttackThrust:     int64(innerUnit.MeleeAttackThrust),
+		MeleeAtkENConsump:     int64(innerUnit.MeleeAtkEnConsump),
+		CloseRangeAssist:      int64(innerUnit.CloseRangeAssist),
+		LogRangeAssist:        int64(innerUnit.LogRangeAssist),
+		Weight:                int64(innerUnit.Weight),
+		CreatedAt:             time.UnixMilli(innerUnit.CreatedAt),
+	}
 }
