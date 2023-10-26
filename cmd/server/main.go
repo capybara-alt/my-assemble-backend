bfec4147c962de30fdde4dc684f417620eacc3e3
Reinker <rencker3@gmail.com>
2023-10-27 00:59:31 +0900

    fix lint error

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -45,6 +45,7 @@ func main() {
 	))
 	mux.Handle(myassemblyv1connect.NewValidationUnitSchemaServiceHandler(
 		handler.NewValidationUnitSchemaHandler(validationUnitSchemaRepo, logger),
+	))
 	mux.Handle(myassemblyv1connect.NewAssemblyServiceHandler(
 		handler.NewAssemblyHandler(assemblyRepo, logger),
 		interceptors,
